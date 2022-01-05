32a53af855ff90c0be57309182753f906c1f1b7e
Evanbowl <evansbowl@gmail.com>
2022-01-05 18:35:04 -0500

    Add OSPanic

--- include/revolution/os.h
+++ include/revolution/os.h
@@ -9,6 +9,7 @@ extern "C" {
 
 void OSReport(const char *, ...);
 void OSFatal(u32 *fg, u32 *bg, const char *str, ...);
+void OSPanic(const char* file, int line, const char* msg, ...);
 
 #ifdef __cplusplus
 }
